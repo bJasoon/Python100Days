a4ebefe4e9eb05b708a01489724574bbb8e2eaea
bJasoon <balete.jason27@gmail.com>
2024-11-09 14:37:19 +0800

    Added Reboorg's World Maze

--- Day 6/practice_day6.py
+++ Day 6/practice_day6.py
@@ -133,5 +133,22 @@ my_function()
 #         jump()
 #     else:
 #         move()
-    
+#=====================================
+# Reboorg's World Maze
+# There is a possibility of an infinite loop within this. Check the udemy course for details
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+    
