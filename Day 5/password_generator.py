e5e052110f9f8d6b87b0401ca501047b2910e1dd
bJasoon <balete.jason27@gmail.com>
2024-11-09 14:34:21 +0800

    Fixed password_generator and added day 6

--- Day 5/password_generator.py
+++ Day 5/password_generator.py
@@ -3,7 +3,6 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-allchar = [letters,numbers,symbols]
 
 # leters = 52, numbers 10, symbols 9
 
@@ -13,52 +12,26 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 generatedPassword = []
+finalPassword = ""
 checkLetterLimit = 0
 checkSymbolLimit = 0
 checkNumberLimit = 0
 
-for currentIteration in range(0, nr_letters+nr_symbols+nr_numbers):
-    chooseCharType = random.randint(0,2)
+for currentIteration  in range(0, nr_letters+nr_symbols+nr_numbers):
+    if checkLetterLimit < nr_letters:
+        generatedPassword.append(random.choice(letters))
+        checkLetterLimit += 1
+    elif checkNumberLimit < nr_numbers:
+        generatedPassword.append(random.choice(numbers))
+        checkNumberLimit += 1
+    elif checkSymbolLimit < nr_symbols:
+        generatedPassword.append(random.choice(symbols))
+        checkSymbolLimit += 1
 
-    if chooseCharType == 0:
-        if checkLetterLimit >= nr_letters:
-            if checkNumberLimit >= nr_numbers:
-                generatedPassword.append(random.choice(symbols))
-                checkSymbolLimit += 1
-            else:
-                generatedPassword.append(random.choice(numbers))
-                checkNumberLimit += 1
-        else:
-            generatedPassword.append(random.choice(letters))
-            checkLetterLimit += 1
-        
-
-    elif chooseCharType == 1:
-        if checkNumberLimit >= nr_numbers:
-            if checkLetterLimit >= nr_letters:
-                generatedPassword.append(random.choice(symbols))
-                checkSymbolLimit += 1
-            else:
-                generatedPassword.append(random.choice(letters))
-                checkLetterLimit += 1
-        else:
-            generatedPassword.append(random.choice(numbers))
-            checkNumberLimit += 1
-
-    elif chooseCharType == 2:
-        if checkSymbolLimit >= nr_symbols:
-            if checkLetterLimit >= nr_letters:
-                generatedPassword.append(random.choice(numbers))
-                checkNumberLimit += 1
-            else:
-                generatedPassword.append(random.choice(letters))
-                checkLetterLimit += 1
-        else:    
-            generatedPassword.append(random.choice(symbols))
-            checkSymbolLimit += 1
-
+random.shuffle(generatedPassword) # This random function is the only thing missing
+finalPassword = "".join(generatedPassword)
     
-print(f'Your generated password is {"".join(generatedPassword)}')
+print(f'Your generated password is {finalPassword}')
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
@@ -66,6 +39,9 @@ print(f'Your generated password is {"".join(generatedPassword)}')
 
 # Hard Level - Order of characters randomised:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+#===========================================================
+#first try (not working)
 
 # for currentIteration in range(0, num_of_characters_in_pw):
 #     chooseCharType = random.randint(0,2)
@@ -83,3 +59,45 @@ print(f'Your generated password is {"".join(generatedPassword)}')
 #         checkNumberLimit += 1
 #     else:
 #         currentIteration -= 1
+
+#===========================================================
+#second convuloted solution
+# for currentIteration in range(0, nr_letters+nr_symbols+nr_numbers):
+#     chooseCharType = random.randint(0,2)
+
+#     if chooseCharType == 0:
+#         if checkLetterLimit >= nr_letters:
+#             if checkNumberLimit >= nr_numbers:
+#                 generatedPassword.append(random.choice(symbols))
+#                 checkSymbolLimit += 1
+#             else:
+#                 generatedPassword.append(random.choice(numbers))
+#                 checkNumberLimit += 1
+#         else:
+#             generatedPassword.append(random.choice(letters))
+#             checkLetterLimit += 1
+        
+
+#     elif chooseCharType == 1:
+#         if checkNumberLimit >= nr_numbers:
+#             if checkLetterLimit >= nr_letters:
+#                 generatedPassword.append(random.choice(symbols))
+#                 checkSymbolLimit += 1
+#             else:
+#                 generatedPassword.append(random.choice(letters))
+#                 checkLetterLimit += 1
+#         else:
+#             generatedPassword.append(random.choice(numbers))
+#             checkNumberLimit += 1
+
+#     elif chooseCharType == 2:
+#         if checkSymbolLimit >= nr_symbols:
+#             if checkLetterLimit >= nr_letters:
+#                 generatedPassword.append(random.choice(numbers))
+#                 checkNumberLimit += 1
+#             else:
+#                 generatedPassword.append(random.choice(letters))
+#                 checkLetterLimit += 1
+#         else:    
+#             generatedPassword.append(random.choice(symbols))
+#             checkSymbolLimit += 1
