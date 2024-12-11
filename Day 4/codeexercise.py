fada76d249523b6c60cbbcd543d61a4778551f2b
bJasoon <81162679+bJasoon@users.noreply.github.com>
2024-12-11 13:02:39 +0800

    Delete Day 4 directory

--- Day 4/codeexercise.py
+++
@@ -1,9 +0,0 @@
-#===================================================
-#Code Exercise
-import random
-
-friends = ["Alice", "Bob", "Charlie", "David", "Emmanuel"]
-
-print(friends[random.randint(0, 4)]) #My Solution
-
-print(random.choice(friends))# Suggested solution (random.choice is a function that randomly selects from a sequential datatype)
