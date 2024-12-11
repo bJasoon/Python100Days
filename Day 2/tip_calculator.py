a05de2a8f461ffdcf3c7b75ed969aef6bfc04961
bJasoon <81162679+bJasoon@users.noreply.github.com>
2024-12-11 13:02:05 +0800

    Delete Day 2 directory

--- Day 2/tip_calculator.py
+++
@@ -1,8 +0,0 @@
-print("welcome to the Tip Calculator!")
-
-totatBill = float(input("What was the total bill?\n$"))
-tipPercentage = int(input("How much tip would you like to give? 10, 12, or 15?\n"))
-peopleSplit = int(input("How many people will split the bill?\n"))
-eachBill = round(((totatBill * ((tipPercentage / 100) + 1)) / peopleSplit), 2)
-
-print(f"Each person will pay: ${eachBill}")
